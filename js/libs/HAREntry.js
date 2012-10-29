298bd0b11dab53b9060c807c191fbca0d1b1144d
Eric J. Duran <eric.duran7@gmail.com>
2012-10-28 20:52:27 -0400

    Cleanup

--- js/libs/HAREntry.js
+++ js/libs/HAREntry.js
@@ -1,9 +1,19 @@
-
-// See http://groups.google.com/group/http-archive-specification/web/har-1-2-spec
-// for HAR specification.
+/**
+ * HAREntry
+ *
+ * Represents a single request in in the HAR file. Most HAR files will contain
+ * multiple requests.
+ *
+ * @author Eric J. Duran
+ * @see http://groups.google.com/group/http-archive-specification/web/har-1-2-spec
+ */
 var HAREntry = (function HAREntryClosure() {
   'use strict';
 
+  /**
+   * A single entry request.
+   * @constructor
+   */
   function HAREntry(entry, id, data) {
     this._entry = entry;
     this._id = id;
@@ -11,8 +21,8 @@ var HAREntry = (function HAREntryClosure() {
     // Response
     this.status = this._entry.response.status;
     this.statusText = this._entry.response.statusText;
-    this.mimeType = this._entry.response.content.mimeType
-    this.receive = this._entry.timings.receive
+    this.mimeType = this._entry.response.content.mimeType;
+    this.receive = this._entry.timings.receive;
     this.receiveTime = this._entry.timings.receive + "ms";
 
     // Request
@@ -51,8 +61,10 @@ var HAREntry = (function HAREntryClosure() {
     getFolder: function () {
       var path = this.parsedURL.path;
       var indexOfQuery = path.indexOf("?");
-      if (indexOfQuery !== -1)
+      if (indexOfQuery !== -1) {
           path = path.substring(0, indexOfQuery);
+      }
+
       var lastSlashIndex = path.lastIndexOf("/");
       return lastSlashIndex !== -1 ? path.substring(0, lastSlashIndex) : "";
     },
@@ -73,10 +85,13 @@ var HAREntry = (function HAREntryClosure() {
 
     getSize: function () {
       // TODO: Fixme, this doesn't correctly pick up items from cache.
-      if (this._entry.cached)
+      if (this._entry.cached) {
         return 0;
-      if (this._entry.response.status === 304) // Not modified
+      }
+      if (this._entry.response.status === 304) {
+        // Not modified
         return Number.bytesToString(this._entry.response.headersSize);
+      }
 
       return Number.bytesToString(this._entry.request.headersSize + this._entry.request.bodySize);
     },
@@ -127,8 +142,7 @@ var HAREntry = (function HAREntryClosure() {
       graph.receiving_right = ''; // TODO: Fixme
       return graph;
     }
-  }
+  };
 
   return HAREntry;
 })();
-
