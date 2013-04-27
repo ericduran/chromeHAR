efcbecc903c3f92bf561bcb5db98644ac980138e
Eric J. Duran <eric.duran7@gmail.com>
2013-04-27 11:27:40 -0400

    Updating Gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-compass");
-  grunt.loadNpmTasks("grunt-contrib-watch");
 
   // Default task.
   grunt.registerTask("default", ["compass", "jshint"]);
