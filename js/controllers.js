805d3e37a7c7c4669461af4a3608824ab99d89f7
Declan Conlon <dconlon@atlassian.com>
2013-04-29 11:59:50 +1000

    Multi-Page Support: Issue #1
    First attempt at supporting multiple pages for a single HAR.

--- js/controllers.js
+++ js/controllers.js
@@ -4,32 +4,70 @@
 angular.module('net', ['net.dnd']).controller('NetworkCtrl', function ($scope) {
   'use strict';
 
-  $scope.entries = $scope.entries || [];
-  $scope.data = '';
-  $scope.startedTime = '';
-  $scope.pageTimings = {};
+  $scope.segments = 12; // Hard-coded number of segments
+  $scope.pages = $scope.pages || []; // Page -> Entry mapping
+  $scope.entries = $scope.entries || []; // Entries
+  $scope.data = {}; // Global data
+
   $scope.checked = false;
   $scope.tab = '1';
   $scope.sI = 'all';
   $scope.selectedEntry = null;
 
-  $scope.updateHar = function(data) {
-    $scope.transfer = 0;
-    var entries = data.log.entries;
-    data.count = data.log.entries.length;
-    delete data.log.entries;
-    $.each(entries, function(i, entry) {
-      entries[i] = new HAREntry(entry, i, data);
-      $scope.transfer += entries[i].getRawContentSize();
+  $scope.updateHar = function(new_data) {
+    // Reset data
+    var data = {};
+    data.lastOnLoad = 0;
+
+    // Handle pages
+    var pages = new_data.log.pages;
+    var pageidxs = {};
+    $.each(pages, function(i, pg) {
+      pageidxs[pg.id] = i;
+      pg.startTime = new Date(pg.startedDateTime).getTime();
+      // Set last onLoad (upper bound on scale)
+      if( !data.lastOnLoad || data.lastOnLoad < pg.pageTimings.onLoad ) {
+         data.lastOnLoad = pg.pageTimings.onLoad;
+      }
+      pg.transfer = 0; // Reset transfer size
     });
+
+    // Handle entries
+    var entries = new_data.log.entries;
+    delete new_data.log.entries;
+    $.each(entries, function(i, entry) {
+      var pg = pages[pageidxs[entry.pageref]];
+      entries[i] = new HAREntry(entry, i, pg.startTime, data);
+      if( !pg.entries ) {
+         pg.entries = [];
+      }
+      pg.entries.push(entries[i]); // Record reference
+      pg.transfer += entries[i].getRawContentSize();
+    });
+
+    $.each(pages, function(i, pg) {
+      pg.transfer = Number.bytesToString(pg.transfer);
+      pg.count = 0;
+      if( pg.entries ) {
+         pg.count = pg.entries.length;
+      }
+      pg.load_event_left = (pg.pageTimings.onLoad/data.lastOnLoad)*100;
+      if( pg.pageTimings.onContentLoad ){
+         pg.content_load_left = (pg.pageTimings.onContentLoad/data.lastOnLoad)*100;
+      } else {
+         pg.content_load_left = 0;
+      }
+    });
+
+
+    // Latch values
     $scope.entries = entries;
+    $scope.pages = pages;
     $scope.checked = true;
     $scope.data = data;
-    $scope.startedTime = new Date(data.log.pages[0].startedDateTime).getTime();
-    $scope.pageTimings = data.log.pages[0].pageTimings;
-    $scope.pageTimings.section = $scope.pageTimings.onLoad / 12;
-    $scope.transfer = Number.bytesToString($scope.transfer);
-    $scope.labels = $scope.setLabels($scope.pageTimings.section);
+
+    // Create labels for segments
+    $scope.labels = $scope.setLabels($scope.data.lastOnLoad/$scope.segments);
   };
 
   $scope.setLabels = function(section) {
